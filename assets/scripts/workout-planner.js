5bccc31abfac299c1a48fd9159f1502e303158ff
razifp <muhammadrazif.pramuda@idemia.com>
2024-07-04 13:12:02 +0700

    js feature done

--- assets/scripts/workout-planner.js
+++ assets/scripts/workout-planner.js
@@ -32,7 +32,22 @@ const predefinedExercises = [
    new Exercise(
       "Bench press",
       "assets/img/bg-1",
-      1,
+      4,
+   ),
+   new Exercise(
+      "Squat",
+      "assets/img/bg-1",
+      8,
+   ),
+   new Exercise(
+      "Deadlift",
+      "assets/img/bg-1",
+      6,
+   ),
+   new Exercise(
+      "Lateral raise",
+      "assets/img/bg-1",
+      2,
    ),
 ]
 const exerciseWorkouts =[
@@ -59,7 +74,7 @@ function buildSetCard(){
 
 
 function buildExerciseWorkoutCard(){
-   const exWorkCard = buildDivWithClasses("exwork-card", "container", "p-4");
+   const exWorkCard = buildDivWithClasses("exwork-card", "container", "p-4" , "my-4");
    return exWorkCard;
 }
 
@@ -115,7 +130,7 @@ function renderSetTableHeader(underElem){
 }
 
 function renderExerciseWorkout(exerciseWorkout, eWNum,  underElem){
-   const exWorkCard = buildExerciseWorkoutCard();
+   const exWorkCard = underElem.appendChild(buildExerciseWorkoutCard());
 
    // exercise details
    const exercise = exWorkCard.appendChild(document.createElement("h2"));
@@ -129,12 +144,11 @@ function renderExerciseWorkout(exerciseWorkout, eWNum,  underElem){
    exerciseWorkout.sets.forEach((set, idx)=>{
       renderSet(set, idx, eWNum, setContainer);
    })
-   underElem.append(exWorkCard);
 
    // add set button
    const addSetButton = exWorkCard.appendChild(document.createElement("button"));
    addSetButton.textContent = "Add Set";
-   addSetButton.classList.add("btn", "btn-primary");
+   addSetButton.classList.add("btn", "btn-secondary");
    addSetButton.onclick = () => {
       let lastSet = exerciseWorkout.sets[exerciseWorkout.sets.length -1];
       if (!lastSet){
@@ -146,10 +160,155 @@ function renderExerciseWorkout(exerciseWorkout, eWNum,  underElem){
    }
 }
 
+function renderSelectExercise(){
+   // get container to render
+   const renderArea= document.getElementById("select-exercise-container");
+   if (!renderArea){
+      console.log("cannot find root node");
+      return
+   }
+   // render
+   // <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+   //             <option selected>Select exercise</option>
+   //             <option value="1">One</option>
+   //             <option value="2">Two</option>
+   //             <option value="3">Three</option>
+   //          </select>
+   const select = renderArea.appendChild(document.createElement("select"))
+   select.classList.add("form-select", "form-select-lg", "mb-3")
+   // so it can referenced later
+   select.id = "select-exercise";
+   // append first option
+   const firstOption = select.appendChild(document.createElement("option"));
+   firstOption.innerHTML = "Select exercise"
+   // append options based on predefined exercises
+   predefinedExercises.forEach((cur, idx)=>{
+      const option = select.appendChild(document.createElement("option"));
+      option.innerHTML = cur.name;
+      // set value to idx so it can be used later
+      option.value = idx
+   })
+}
+
+
+function addExercise(){
+   const selectInput= document.getElementById("select-exercise");
+   if (!selectInput){
+      console.log("cannot find select exercise input");
+      return
+   }
+
+   const selectedExerciseIdx = Number.parseInt(selectInput.value)
+   console.log(`selected ex idx: ${selectedExerciseIdx}`);
+   // check value and within range
+   if(selectedExerciseIdx >-1 && selectedExerciseIdx<predefinedExercises.length){
+      // add exercise at input
+      const exercise = predefinedExercises[selectedExerciseIdx];
+      console.log(`Adding ex workout: ${exercise.name}`);
+      exerciseWorkouts.push(new ExerciseWorkout(
+         exercise
+      ));
+   }
+   render()
+}
+
+function calculateWorkoutSummary(){
+   const totalSets = exerciseWorkouts.reduce((prev, cur)=>{
+      return prev + cur.sets.length;
+   },0);
+   // volume is weight * reps
+   //  gets volume for each exWorkout
+   const exWorkoutVolumes = exerciseWorkouts.map((cur)=>{
+      return cur.sets.reduce((prev, cur)=>{
+         return prev + cur.weight * cur.reps;
+      },0);
+   })
+
+   // assume 1 set takes 2 minutes, with 3 minute rest time
+   const estimatedWorkoutTimeMinutes = totalSets * 2 + totalSets * 3;
+   // from google
+   const caloriesBurnedPerHour = 360
+
+   const totalCalories = (360/60) * estimatedWorkoutTimeMinutes;
+
+   return {
+      totalSets: totalSets,
+      totalVolume: exWorkoutVolumes.reduce((prev,cur)=>{return prev+cur},0),
+      estimatedWorkoutTimeMinutes: estimatedWorkoutTimeMinutes,
+      totalCalories: totalCalories
+   };
+}
+
+// append 1 row in summry field
+function appendSummaryField(label, value, underCard){
+   const row = underCard.appendChild(
+      buildDivWithClasses(
+         "d-flex", "flex-row", "justify-content-between", "summary-field",
+         "my-2"
+      )
+   );
+   const labelDiv = row.appendChild(buildDivWithClasses());
+   labelDiv.innerHTML = label;
+   const valueDiv = row.appendChild(buildDivWithClasses());
+   valueDiv.innerHTML = value;
+}
+
+
+// for appending h2
+function appendSummaryH2(value, underCard){
+   const row = underCard.appendChild(
+      buildDivWithClasses(
+         "d-flex", "flex-row", "justify-content-center",
+         "my-2"
+      )
+   );
+   const valueDiv = row.appendChild(document.createElement("h2"));
+   valueDiv.textContent = value;
+}
+
+function finishWorkout(){
+   // get container to show workout summary
+   const renderArea = document.getElementById("finish-workout-container");
+   if(!renderArea) {
+      console.log("Error on finsih workout, cannot find render container");
+      return;
+   }
+   // remove button
+   renderArea.innerHTML = "";
+   // disable add exercise button
+   const addExerciseBtn = document.getElementById("add-exercise");
+   addExerciseBtn.classList.add("disabled")
+   //
+   const workoutSummary = calculateWorkoutSummary()
+   console.log(workoutSummary)
+   // render summary table
+   const summaryCard = renderArea.appendChild(
+      buildDivWithClasses(
+         "summary-card",
+         "d-flex", 
+         "flex-column",
+         "p-3",
+         "my-5"
+      )
+   );
+   // header
+   const summaryHeader = summaryCard.appendChild(document.createElement("h2"));
+   summaryHeader.textContent = "Keep up the good work!"
+   // summary fields
+   appendSummaryField("Total Sets", workoutSummary.totalSets, summaryCard);
+   appendSummaryField("Total Volume", `${workoutSummary.totalVolume} kgs`, summaryCard);
+   const totalMins = workoutSummary.estimatedWorkoutTimeMinutes;
+   const hours = Math.floor(totalMins / 60);
+   const min = totalMins %60
+   appendSummaryField("Estimated Workout Time", `${hours} hour(s) ${min} minute(s)`, summaryCard);
+
+   appendSummaryH2(`You burned ${workoutSummary.totalCalories} calories!`, summaryCard);
+}
+
 // renders data into root div
 function render(){
    console.log("rendering..");
-   const renderArea= document.getElementById("app-root");
+   const renderArea= document.getElementById("workout-container");
    if (!renderArea){
       console.log("cannot find root node");
       return
@@ -163,4 +322,13 @@ function render(){
    });
 }
 
+// add button events
+// add exercise button
+const addExerciseBtn = document.getElementById("add-exercise");
+addExerciseBtn.addEventListener("click", (e)=>{addExercise()})
+// finish workout buttone
+const finsihExerciseBtn = document.getElementById("finish-workout-btn");
+finsihExerciseBtn.addEventListener("click", (e)=>{finishWorkout()})
+// render
+renderSelectExercise()
 render();
